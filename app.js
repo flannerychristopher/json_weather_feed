51c490097c08d4c8bebd631339fcb1fa8b068c14
flan the dev <flanthedev@gmail.com>
2017-08-20 22:21:14 +0800

    set http get

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-http = require('http');
+const http = require('http');
 const apiKey = 'cacdf29dc2be47d484a105606152306';
 const location = 'Boston';
 
@@ -7,4 +7,17 @@ function printWeather(location, temperature, condition) {
   console.log(message);
 }
 
-const request = http.get(`http://api.apixu.com/v1/current.json?key=${apiKey}&q=${location}`)
+const request = http.get(`http://api.apixu.com/v1/current.json?key=${apiKey}&q=${location}`, (res) => {
+  console.dir(res.statusCode);
+
+  let rawData = '';
+  res.on('data', (chunk) => {
+    rawData += chunk;
+  });
+
+  res.on('end', () => {
+    console.log(rawData);
+    const parsedData = JSON.parse(rawData);
+    console.log(parsedData);
+  })
+});
