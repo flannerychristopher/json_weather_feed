09103c5ff699abe9d2370ae798616111de595359
flan the dev <flanthedev@gmail.com>
2017-08-20 22:34:05 +0800

    parse and console log json data

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const http = require('http');
 const apiKey = 'cacdf29dc2be47d484a105606152306';
-const location = 'Boston';
+// const location = 'Boston';
 
 function printWeather(location, temperature, condition) {
   message = `${location}, ${temperature}, ${condition}`;
   console.log(message);
 }
 
-const request = http.get(`http://api.apixu.com/v1/current.json?key=${apiKey}&q=${location}`, (res) => {
-  console.dir(res.statusCode);
+function getWeather(location) {
+  const request = http.get(`http://api.apixu.com/v1/current.json?key=${apiKey}&q=${location}`, (res) => {
+    console.dir(res.statusCode);
 
-  let rawData = '';
-  res.on('data', (chunk) => {
-    rawData += chunk;
+    let rawData = '';
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
+
+    res.on('end', () => {
+      const parsedData = JSON.parse(rawData);
+      // console.log(parsedData);
+      // console.log(parsedData.location.name);
+
+      printWeather(parsedData.location.name, parsedData.current.temp_c, parsedData.current.condition.text);
+    })
   });
+}
 
-  res.on('end', () => {
-    console.log(rawData);
-    const parsedData = JSON.parse(rawData);
-    console.log(parsedData);
-  })
-});
+get
