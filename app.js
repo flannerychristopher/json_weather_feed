ed0656325e8c82f1773185ecaebf45594b88015a
flan the dev <flanthedev@gmail.com>
2017-08-21 16:55:52 +0800

    make separate module for weather.get

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
-const http = require('http');
-
-const reqData = {
-  apiKey: 'cacdf29dc2be47d484a105606152306',
-  url: 'http://api.apixu.com/v1/',
-}
-
-function printWeather(location, temperature, condition) {
-  message = `${location}, ${temperature}, ${condition}`;
-  console.log(message);
-}
-
-function getWeather(location) {
-  const request = http.get(`${reqData.url}current.json?key=${reqData.apiKey}&q=${location}`, (res) => {
-
-    let rawData = '';
-    res.on('data', (chunk) => {
-      rawData += chunk;
-    });
-
-    res.on('end', () => {
-      const parsedData = JSON.parse(rawData);
-      printWeather(parsedData.location.name, parsedData.current.temp_c, parsedData.current.condition.text);
-    });
-  });
-}
+const weather = require('./weather');
 
 // const cities = ['Singapore', 'Boston', 'Tokyo'];
-const cities = process.argv.slice(2);
-cities.forEach(getWeather);
+const search = process.argv.slice(2).join('_').replace(' ', '_');
+weather.get(search);
